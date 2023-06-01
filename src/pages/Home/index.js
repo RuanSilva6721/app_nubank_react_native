dc9c8ac2916fe0c5d46e7e110d0965c5fa711017
Ruan Silva <ruan.silva@ditech.com.br>
2023-06-01 08:19:47 -0300

    creating component of movements

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,14 +1,47 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
 import Header from '../../components/Header';
 import Balance from '../../components/Balance';
+import Movements from '../../components/Movements';
+
+const list = [
+    {
+    id: 1,
+    label: 'Boleto de Luz',
+    value: '390,90',
+    data: '01/06/2023',
+    type: 0
+    },
+    {
+      id: 1,
+      label: 'Pix Cliente Jorge',
+      value: '140,90',
+      data: '02/06/2023',
+      type: 1
+    },
+    {
+      id: 1,
+      label: 'Salário',
+      value: '330,90',
+      data: '01/03/2023',
+      type: 1
+    },
+
+]
+
 
 export default function Home() {
   return (
     <View style={styles.container}>
     <Header name ="Ruan Felipe"/>
     <Balance balance="100,23" spent="23,89"/>
-      <Text>Home</Text>
+    <Text style={styles.title}>Últimas Movimentações</Text>
+    <FlatList 
+    style={styles.list} 
+    data={list} keyExtractor={(item) => String(item.id)}
+    showsVerticalScrollIndicator={false}
+    renderItem={({item})=> <Movements data={item} />} 
+    />
     </View>
   );
 }
@@ -17,5 +50,17 @@ const styles = StyleSheet.create({
     container:{
         flex: 1,
         backgroundColor: '#fafafa',
+    },
+    title:{
+      fontSize: 18,
+      fontWeight: 'bold',
+      marginLeft: 14,
+      marginRight:14,
+      marginTop: 14,
+
+    },
+    list:{
+      marginStart:14,
+      marginEnd: 14
     }
 })
